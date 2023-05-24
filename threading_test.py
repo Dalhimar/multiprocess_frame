20fe814c5efed310d7bc32b18b576c69d08d382b
Dalhimar <133586389+Dalhimar@users.noreply.github.com>
2023-05-24 11:45:17 -0400

    Update threading_test.py
    Included arguments to calculate the maximum number of threads based on an arbitrary value, 4, to determine the number of threads to create.

--- threading_test.py
+++ threading_test.py
@@ -1,4 +1,11 @@
 import multiprocessing
+import math
+
+def math_function(input):
+    """Math function to calculate potential_threads and max_threads"""
+    potential_threads = math.floor(input / 4)
+    max_threads = min(potential_threads, num_cores - 1)
+    return potential_threads, max_threads
 
 def worker(process_id):
     """Function to be executed by each process"""
@@ -8,8 +15,14 @@ if __name__ == "__main__":
     num_cores = multiprocessing.cpu_count()
     print(f"Number of CPU cores available: {num_cores}")
 
+    input_value = 20  # Set your desired input value here
+
+    potential_threads, max_threads = math_function(input_value)
+    print(f"Potential threads: {potential_threads}")
+    print(f"Max threads: {max_threads}")
+
     processes = []
-    for i in range(num_cores-1):
+    for i in range(max_threads):
         process = multiprocessing.Process(target=worker, args=(i,))
         processes.append(process)
 
@@ -20,4 +33,3 @@ if __name__ == "__main__":
     # Wait for all processes to finish
     for process in processes:
         process.join()
-
